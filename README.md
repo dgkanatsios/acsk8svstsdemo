4b976970a65c9b7fea9a36d660cce06eb9836b21
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2017-05-29 15:36:39 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Kubernetes on Azure Container Service Continuous Integration/Continuous Deployment demo with Visual Studio Team Services
+  
---
+++ service-a/server.js
@@ -0,0 +1,61 @@
+var os = require('os');
+var request = require('request');
+var morgan = require('morgan');
+var express = require('express');
+var redis;
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(morgan("dev"));
+
+// application -------------------------------------------------------------
+app.get('/', function (req, res) {
+    console.log('/ request');
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// api ------------------------------------------------------------
+app.get('/api', function (req, res) {
+    // Increment requestCount each time API is called
+    if (!redis) { redis = connectToCache(); }
+    redis.incr('requestCount', function (err, reply) {
+        if(err) console.log(err);
+        var requestCount = reply;
+    });
+
+    console.log('/api request');
+
+    // Invoke service-b
+    request('http://service-b', function (error, response, body) {
+        if(error) console.log(error);
+        res.send('Hello from service A running on ' + os.hostname() + ' and ' + body);
+    });
+}); 
+
+app.get('/metrics', function (req, res) {
+    if (!redis) { redis = connectToCache(); }
+    console.log('/metrics request');
+    redis.get('requestCount', function (err, reply) {
+        if(err) console.log(err);
+        res.send({ requestCount: reply });
+    });
+});
+
+var port = 80;
+var server = app.listen(port, function () {
+    console.log('Listening on port ' + port);
+});
+
+process.on("SIGINT", () => {
+    process.exit(130 /* 128 + SIGINT */);
+});
+
+process.on("SIGTERM", () => {
+    console.log("Terminating...");
+    server.close();
+});
+
+function connectToCache() {
+    var redis = require('redis').createClient("redis://mycache");
+    return redis;
+}
